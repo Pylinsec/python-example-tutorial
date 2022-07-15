0659353347a86a1e3c1bc7598408b2d7492cdb80
abolbdh <abol.binandeh@gmail.com>
2022-07-15 12:16:04 +0430

    send3

---
+++ Countdown_timer/main.py
@@ -0,0 +1,32 @@
+import time
+import os
+# hours,mins , secs = 0,0,0
+def countdown(t):
+
+    while t:
+        if t >= 3600:
+            hours, t1 = divmod(t,3600)
+            print(hours)
+            mins, secs = divmod(t1, 60)
+            timer = '{:02d}:{:02d}:{:02d}'.format(hours,mins,secs)
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
+
+
+        else:
+            hours = 0
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}:{:02d}'.format(hours,mins,secs)
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
+
+
+        os.system('cls')
+
+    print('Timer completed!')
+
+t = input('Enter the time in seconds: ')
+
+countdown(int(t))
